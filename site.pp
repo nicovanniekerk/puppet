c7ee5dc51e4389dcb096e4bfc1b0bbc1cd678473
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-10 14:07:31 +0200

    testing commit

--- site.pp
+++ site.pp
@@ -1,13 +1,23 @@
 
+
+class showmessage {
+		file {'/usr/local/sbin/message.sh':
+		owner => root, group => root, mode => 0755,
+		content => "ECHO "SUP MOTHER FUCKER"",
+		}
+}
+
+
+
 class toolbox {
+	include showmessage
 	file{ '/usr/local/sbin/puppetsimple.sh':
 		owner => root, group => root, mode => 0755,
 		content => "mkdir /etc/puppet/gitfiles/\ncd /etc/puppet/gitfiles/\ngit init\ngit pull https://github.com/nicovanniekerk/puppet_testing.git",
 
 	    }
 exec {"puppet_simple":
-command => '/bin/sh -c "/usr/local/sbin/puppetsimple.sh"';}
-
+command => '/bin/sh -c "/usr/local/sbin/message.sh"';}
 
 }
 
