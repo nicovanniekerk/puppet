c73122738b2bd77a944eddad99a05372ea18dea0
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-10 14:10:01 +0200

    azscdasd

--- site.pp
+++ site.pp
@@ -10,7 +10,7 @@ class showmessage {
 
 
 class toolbox {
-	include showmessage
+	
 	file{ '/usr/local/sbin/puppetsimple.sh':
 		owner => root, group => root, mode => 0755,
 		content => "mkdir /etc/puppet/gitfiles/\ncd /etc/puppet/gitfiles/\ngit init\ngit pull https://github.com/nicovanniekerk/puppet_testing.git",
@@ -22,6 +22,7 @@ command => '/bin/sh -c "/usr/local/sbin/message.sh"';}
 }
 
 node 'ubuntu.imqs.local' {
+	include showmessage
 	include toolbox
 
 }
