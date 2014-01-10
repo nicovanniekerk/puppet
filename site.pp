03cc1774797c18c1859ee60952cb286cd1ff2402
root <nico.vanniekerk@imqs.co.za>
2014-01-10 04:48:33 -0800

    hallo

--- site.pp
+++ site.pp
@@ -3,7 +3,7 @@
 class showmessage {
 		file {'/usr/local/sbin/message.sh':
 		owner => root, group => root, mode => 0755,
-		content => "echo 'SUP MOTHER FUCKER!!!!!!!!!!!!dvskljxncv.,mxncvk;xnjdgfxzc.vnx cvkn\nljz\xdbnfxz\,m' >> new.txt",
+		content => "echo 'SUP MOTHER FUCKER!!!!!!!!!!!!bitchm' >> new.txt",
 		}
 }
 
