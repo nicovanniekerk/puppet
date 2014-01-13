23e3b997c9ced617ba75c4e0f30483f72f575b1d
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-13 15:17:14 +0200

    asdasd

--- site.pp
+++ site.pp
@@ -4,7 +4,7 @@ class toolbox {
 	
 	file{ '/usr/local/sbin/puppetsimple.sh':
 		owner => root, group => root, mode => 0755,
-		content => "mkdir /etc/puppet/geel",
+		content => "mkdir /etc/puppet/darron",
 
 	    }
 exec {"puppet_simple":
