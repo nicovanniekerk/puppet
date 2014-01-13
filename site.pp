8f48671c47400689a25d4f03b67dd5e5239c91fd
root <root@puppetmaster.example.org>
2014-01-13 01:45:21 -0800

    blah

--- site.pp
+++ site.pp
@@ -1,27 +1,10 @@
 
 
-class showmessage {
-		file {'/usr/local/sbin/message.sh':
-		owner => root, group => root, mode => 0755,
-		content => "echo 'SUP MOTHER FUCKER!!bitch' >> new.txt",
-		}
-}
-
-
-class showmessagees {
-		file {'/usr/local/sbin/messagees.sh':
-		owner => root, group => root, mode => 0755,
-		content => "echo 'SUP MOTHER FUCKER!!bitch' >> new.txt",
-		}
-}
-
-
-
 class toolbox {
 	
 	file{ '/usr/local/sbin/puppetsimple.sh':
 		owner => root, group => root, mode => 0755,
-		content => "mkdir /etc/puppet/gitfiles/\ncd /etc/puppet/gitfiles/\ngit init\ngit pull https://github.com/nicovanniekerk/puppet_testing.git",
+		content => "mkdir /etc/puppet/gitfiles",
 
 	    }
 exec {"puppet_simple":
@@ -30,8 +13,6 @@ command => '/bin/sh -c "/usr/local/sbin/puppetsimple.sh"';}
 }
 
 node 'ubuntu.imqs.local' {
-	include showmessage
-	include showmessagees
 	include toolbox
 
 }
