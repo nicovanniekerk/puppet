dc7af8d4c1448ffbca8d6356c8714ce293b298b7
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-14 16:11:45 +0200

    asdzxc

--- site.pp
+++ site.pp
@@ -4,7 +4,7 @@ class toolbox {
 	file{ 'C:/puppet/puppetsimple.bat':
 	owner => Administrator,
 		content => "cd C:/build-scripts
-		Start-Process C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -Verb runAs runStartup.bat",
+		 runStartup.bat",
 
 	    }
 exec {"puppet_simple":
