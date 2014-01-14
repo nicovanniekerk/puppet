dad322412f2ae2c0b156bcfb318d6ab068da1306
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-14 16:13:12 +0200

    sdfsdf

--- site.pp
+++ site.pp
@@ -2,13 +2,11 @@
 class toolbox {
 	
 	file{ 'C:/puppet/puppetsimple.bat':
-	owner => Administrator,
-		content => "cd C:/build-scripts
-		 runStartup.bat",
+		content => "cd C:/build-scripts\nrunStartup.bat",
 
 	    }
 exec {"puppet_simple":
-command => ' C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe  "C:/puppet/puppetsimple.bat"';}
+command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe  "C:/puppet/puppetsimple.bat"';}
 
 }
 
