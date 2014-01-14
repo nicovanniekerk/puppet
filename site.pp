3263eb2eab3b4a1bc5d7656f6f43110a3162de5f
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-14 15:22:04 +0200

    dsfgfsdf

--- site.pp
+++ site.pp
@@ -2,12 +2,12 @@
 class toolbox {
 	
 	file{ 'C:/puppet/puppetsimple.bat':
-		owner => 'Administrators',
+		owner => 'Administrator',
 		content => "cd C:/build-scripts\njruby startup_script.rb",
 
 	    }
 exec {"puppet_simple":
-command => 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe  "C:/puppet/puppetsimple.bat"';}
+command => 'cmd /c  "C:/puppet/puppetsimple.bat"';}
 
 }
 
