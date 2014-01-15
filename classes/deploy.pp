a9e0e0451dd4231672e4657079999e2653478751
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-15 15:45:40 +0200

    asdasd

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -1,11 +1,5 @@
 class deployServises {
 
-	
-	file{ "C:/puppet":
-	recurse => true,
-    source => "puppet:///puppetfiles",
-	    }
-
 	exec { 'switch':
 	command => 'cmd /k "runStartup.bat"',
 	cwd =>  'C:/build-scripts',
@@ -13,6 +7,9 @@ class deployServises {
 	path => $::path,
 	}
 		
-
+	file{ "C:/puppet":
+		recurse => true,
+		source => "puppet:///puppetfiles",
+	}
 
 }
