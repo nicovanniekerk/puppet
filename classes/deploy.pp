abed031e69c9a30ef1a31d318d06a157adb86702
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-15 16:11:42 +0200

    dsfsdf

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -6,7 +6,7 @@ class deployServises {
 	logoutput => true,
 	path => $::path,
 	refreshonly => true,
-	subscribe => File['C:/puppet/services/web-services-1.0.1-SNAPSHOT.zip'],
+	subscribe => File['C:/puppet/build-scripts/runStartup.bat'],
 	}
 		
 	file{ "C:/puppet":
