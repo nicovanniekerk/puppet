ad3c7ac3a6312870e46806b3b442e35518a02bd3
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-15 12:53:19 +0200

    asdfsdf

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -1,10 +1,6 @@
 class deployServises {
-
-		file { 'C:/puppet/start.bat':
-			content => 'cd C:/build-scripts\nrunStartup.bat',
-		}
 		
 	exec { 'run_deploy':
-	command => 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe start.bat';
+	command => 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe C:/build-scripts/runStartup.bat';
 	}
 }
