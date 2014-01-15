beef5c59b76ac34000735b0671c8b2cce6cac691
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-15 13:21:23 +0200

    sdcfxscv

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -1,12 +1,9 @@
 class deployServises {
 		
 	exec { 'switch':
-	command => 'cmd "cd C:/build-scripts"',
-	path => $::path
-	}
-	
-	exec { 'run_deploy':
 	command => 'cmd "runStartup.bat"',
+	cwd =>  'C:/build-scripts'
 	path => $::path
 	}
+
 }
