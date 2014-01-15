7b2a302929c0f4d77605a36e34fc9c05e760b658
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-15 13:02:31 +0200

    sdfsdf

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -1,6 +1,6 @@
 class deployServises {
 		
 	exec { 'run_deploy':
-	command => 'C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe "C:/build-scripts/runStartup.bat"';
+	command => 'cmd /C "C:/build-scripts/runStartup.bat"';
 	}
 }
