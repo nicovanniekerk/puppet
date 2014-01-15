33f165920111aea10fcb263ca47c113e25a97170
nicovaniekerk <nico.vanniekerk@imqs.co.za>
2014-01-15 15:57:44 +0200

    dscfxzdfc

--- classes/deploy.pp
+++ classes/deploy.pp
@@ -5,7 +5,7 @@ class deployServises {
 	cwd =>  'C:/build-scripts',
 	logoutput => true,
 	path => $::path,
-	onlyif => file['C:/puppet/services/web-services.zip'],
+	onlyif => file['C:/puppet/services/web-services-1.0.1-SNAPSHOT.zip'],
 	}
 		
 	file{ "C:/puppet":
