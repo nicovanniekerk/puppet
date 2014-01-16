94d8bc4ec29ab7fcb29b46690b3cc5efac9647f6
navonod <donovan.thatcher@gmail.com>
2014-01-16 15:47:57 +0200

    Pulled the puppet deployment trigger

--- site.pp
+++ site.pp
@@ -3,7 +3,7 @@ import 'classes/*.pp'
 class toolbox {
                 
                 file{ 'C:/puppet/log.txt':
-                                content => ' deploy started 16/01/2014 15:47:26.',
+                                content => ' deploy started 16/01/2014 15:47:57.',
                     }
 
 }
