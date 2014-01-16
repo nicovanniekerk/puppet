da6810b7da45e999f06d54207084fb429b548574
unknown <SYSTEM@DEV004.imqs.local>
2014-01-16 16:22:46 +0200

    Pulled the puppet deployment trigger

--- site.pp
+++ site.pp
@@ -3,7 +3,7 @@ import 'classes/*.pp'
 class toolbox {
                 
                 file{ 'C:/puppet/log.txt':
-                                content => ' deploy started 16/01/2014 15:50:21.',
+                                content => ' deploy started 16/01/2014 16:22:45.',
                     }
 
 }
